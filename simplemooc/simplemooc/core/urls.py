c313ea273529b6e4cc9833c5bcd6762b8cadf073
mamgjunior <mamgjunior@hotmail.com>
2020-09-12 11:42:35 -0300

    Finalizando seção 1 do projeto

--- simplemooc/simplemooc/core/urls.py
+++ simplemooc/simplemooc/core/urls.py
@@ -1,6 +1,9 @@
+from distutils.command.config import config
+
 from django.urls import path
 from .views import *
 
 urlpatterns = [
     path('', home, name="home"),
+    path('contato/', contato, name="contato"),
 ]
