28565cf7bb3f353cf984d151e84b07f5a42af0a8
mamgjunior <mamgjunior@hotmail.com>
2020-09-12 17:21:25 -0300

    Criando o app Courses

---
+++ simplemooc/simplemooc/courses/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Course(models.Model):
+    name = models.CharField(verbose_name="Nome", max_length=100)
+    slug = models.SlugField(verbose_name="Atalho")
+    description = models.TextField(verbose_name="Descrição", blank=True)
+    start_date = models.DateField(verbose_name="Date de Início", null=True, blank=True)
+    image = models.ImageField(upload_to="courses/images", verbose_name="Imagem")
+    created_at = models.DateTimeField(verbose_name="Criado em", auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name="Atualizado em", auto_now=True)
--- simplemooc/simplemooc/settings.py
+++ simplemooc/simplemooc/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -38,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'simplemooc.core',
+    'simplemooc.courses',
 ]
 
 MIDDLEWARE = [
@@ -104,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Fortaleza'
 
 USE_I18N = True
 
@@ -119,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Local onde ficaram os arquivos de imagens
+MEDIA_ROOT = os.path.join(BASE_DIR, 'simplemooc', 'media')
