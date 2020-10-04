6d64796673ae8f538c3ab9165fe596093deb44e8
mamgjunior <mamgjunior@hotmail.com>
2020-10-04 17:06:55 -0300

    Trabalhando com slug como atalho de links

--- simplemooc/simplemooc/courses/views.py
+++ simplemooc/simplemooc/courses/views.py
@@ -12,8 +12,14 @@ def index(request):
     return render(request, template_name, context)
 
 
-def details(request, pk):
-    course = get_object_or_404(Course, pk=pk)
+# def details(request, pk):
+#     course = get_object_or_404(Course, pk=pk)
+#     template_name = "courses/details.html"
+#     context = {"course": course}
+#     return render(request, template_name, context)
+
+def details(request, slug):
+    course = get_object_or_404(Course, slug=slug)
     template_name = "courses/details.html"
     context = {"course": course}
     return render(request, template_name, context)
