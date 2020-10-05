6dd88ebb2f6ecfb202968384cae3af20173cae54
mamgjunior <mamgjunior@hotmail.com>
2020-10-05 20:59:02 -0300

    Validando o formuário

--- simplemooc/simplemooc/courses/views.py
+++ simplemooc/simplemooc/courses/views.py
@@ -21,15 +21,18 @@ def index(request):
 
 def details(request, slug):
     course = get_object_or_404(Course, slug=slug)
+    context = {}
 
     if request.method == "POST":
         form = ContactCourse(request.POST)
+        if form.is_valid():
+            context["is_valid"] = True
+            print(form.cleaned_data)
+            form = ContactCourse()
     else:
         form = ContactCourse()
 
+    context["course"] = course
+    context["form"] = form
     template_name = "courses/details.html"
-    context = {
-        "course": course,
-        "form": form
-    }
     return render(request, template_name, context)
